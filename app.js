330458d1526e752c0200af01053cbdb36b10f742
roxania <groxania@gmail.com>
2022-05-25 09:37:37 -0300

    firts commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express =require('express');
+const app = express();
+app.get('/',(req,res) => {
+    console.log("peticion resivida");
+    res.send("<h1>hola mundo!</h1>");
+})
+app.listen(4000,()=>{
+    console.log("servidor escuchando en el puerto 4000");
+})
